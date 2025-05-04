14894f244affbbb56dad887fa5284be61dcaa7e8
Alinur1 <alinur1160@gmail.com>
2025-05-04 15:16:06 +0600

    ToolDeck 2.0 first project created

---
+++ tooldeck/main.js
@@ -0,0 +1,41 @@
+const path = require('node:path');
+const isDev = require('electron-is-dev');
+
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+    awaitWriteFinish: true,
+  });
+}
+
+const { app, BrowserWindow } = require('electron/main');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
